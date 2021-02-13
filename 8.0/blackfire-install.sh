d1f0a78747e120a941153609e7c3fda91c6f1ec5
Jason McCallister <jason@craftcms.com>
2021-02-13 11:37:53 -0500

    cleanup blackfire installer

--- 8.0/blackfire-install.sh
+++ 8.0/blackfire-install.sh
@@ -1,7 +1,5 @@
 readonly local version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;")
 readonly local system_arch=$(uname -p)
-
-# determine the arch
 arch_sys="amd64"
 case $system_arch in
     "aarch64")
@@ -15,14 +13,22 @@ case $system_arch in
     ;;
 esac
 
-echo "export BLACKFIRE_URL=https://blackfire.io/api/v1/releases/probe/php/linux/$arch_sys/$version" >> /etc/profile
-
+readonly local BLACKFIRE_URL=https://blackfire.io/api/v1/releases/probe/php/linux/$arch_sys/$version
 readonly local ext_dir=$(find /usr/local/lib/php/extensions/ -name "no-debug-non-zts-*")
 readonly local so_path="${ext_dir}/blackfire.so"
-readonly local ext_string="extension=${so_path}"
+readonly local ext_ini_string="extension=${so_path}"
+
+# download the probe
+curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s $BLACKFIRE_URL
+
+# create the temp dir
+mkdir -p /tmp/blackfire
+
+# untar the download
+tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire
 
 # move the .so file into the
-mv /tmp/blackfire/*.so $so_path
+cp /tmp/blackfire/blackfire-*.so $so_path
 
 # load the extension in the ini file
-echo $ext_string > /tmp/ext-blackfire.ini
+echo $ext_ini_string > /tmp/ext-blackfire.ini
